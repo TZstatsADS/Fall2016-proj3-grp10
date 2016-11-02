c913c5e356acf03226c64c22f33271b3f5f19473
caz2114 <caz2114@barnard.edu>
2016-11-02 02:35:27 -0400

    Update base.R

--- doc/base.R
+++ doc/base.R
@@ -12,20 +12,23 @@ library("adabag")
 library("plotly")
 library("gbm")
 
-sift <- fread("./Fall2016-proj3-grp10-master/data/sift_color_texture.csv")
+sift <- fread("./Fall2016-proj3-grp10-master/data/final_feature2.csv")
+#sift<-read.csv("./Fall2016-proj3-grp10-master/data/final_feature2.csv", header=TRUE, sep=",", dec=".", stringsAsFactors=FALSE)
 sift2 <- sift
 
 #use this to refresh the data
 sift <- sift2
+sift <- sift[-1,] #remove first column
 sift <- as.matrix(sift)
-sift <- sift[5001:5035,]
+sift<-t(sift)
 sift <- na.omit(sift)
-sift <- t(sift)
 colnames(sift) <- paste("feature",c(1:ncol(sift)))
-sift <- data.frame(sift)
-sift$class <- ifelse(grepl("chicken",rownames(sift)),1,0)
+sift <- as.data.frame(sift)
+#sift$class <- ifelse(grepl("chicken",rownames(sift)),1,0)
 #sift$class <- factor(sift$class,labels = c("chicken","dog"))
-
+a.vector <- rep(1,1000)
+a.vector <- c(a.vector,rep(0,1000))
+sift$class<- a.vector
 
 this <- colSums(subset(sift,select = -class))
 while(min(this) < 0.0001){
@@ -38,7 +41,7 @@ while(min(this) < 0.0001){
 
 
 #dividing training and test sample
-t_x <- sample(nrow(sift),nrow(sift)*0.15)
+t_x <- sample(nrow(sift),nrow(sift)*0.20)
 X_dataset <- sift[-t_x,]
 X_test_dataset <- sift[t_x,]
 X_train <- subset(sift[-t_x,], select= -class )
@@ -48,7 +51,6 @@ Y_test <- sift$class[t_x]
 
 
 fit.gbm1 <- gbm(class ~ ., data=X_dataset, dist="adaboost", n.tree = 400,shrinkage = 1)
-
 
 pred_gbm <- predict(fit.gbm1, X_test,n.tree=10)
 for(i in 1:length(pred_gbm)){
@@ -78,28 +80,31 @@ sum(pred_gbm==Y_test)/length(Y_test)
 # pred_kern_poly <- predict(model_kern_poly, X_test)
 # sum(pred_kern_poly==Y_test)/length(Y_test)
 # 
-# #kernlab_poly...accuracy rate 68.667% 68%
-# model_kern_line <- ksvm(class~., data = X_dataset,type = "C-svc",kernel = "vanilladot")
-# pred_kern_line <- predict(model_kern_line, X_test)
-# sum(pred_kern_line==Y_test)/length(Y_test)
-# 
-# #tree...accuracy rate 58% 61.3% ... with color 74% ... with texture 75%
-# model_tree <- ctree(class ~ ., data = X_dataset)
-# pred_tree <- predict(model_tree,X_test)
-# sum(pred_tree==Y_test)/length(Y_test)
+#kernlab_poly...accuracy rate 68.667% 68%
+model_kern_line <- ksvm(class~., data = X_dataset,type = "C-svc",kernel = "vanilladot")
+pred_kern_line <- predict(model_kern_line, X_test)
+sum(pred_kern_line==Y_test)/length(Y_test)
+ 
+#tree...accuracy rate 58% 61.3% ... with color 74% ... with texture 75%
+model_tree <- ctree(class ~ ., data = X_dataset)
+pred_tree <- predict(model_tree,X_test)
+sum(pred_tree==Y_test)/length(Y_test)
 
-#adaboost 74% 71% 74% ... with color 84% ... with texture 
-model_adaboost_color_texture <- boosting(formula = class~.,data= X_dataset , boos=TRUE)
-wasave(model_adaboost_color_texture, file = "model_adaboost_color_texture.Rdata")
-pred_adaboost <- predict(model_adaboost_color_texture,X_test)
-sum(pred_adaboost$class==Y_test)/length(Y_test)
 
-model_adaboost$importance
-error_adaboost <- errorevol(model_adaboost,X_test_dataset)
-x<-c(1:100)
-ada_table <- rbind(x,error_adaboost$error)
-ada_table <- t(ada_table)
-ada_table <- as.data.frame(ada_table)
-colnames(ada_table) <- c("iteration","error_rate")
-plot(x=ada_table$iteration,y=ada_table$error_rate)
+
+
+ # # #adaboost 74% 71% 74% ... with color 84% ... with texture 
+ #  model_adaboost_color_texture <- boosting(formula = class~.,data= X_dataset , boos=TRUE)
+ #  save(model_adaboost_color_texture, file = "model_adaboost_color_texture.Rdata")
+ #  pred_adaboost <- predict(model_adaboost_color_texture,X_test)
+ #  sum(pred_adaboost$class==Y_test)/length(Y_test)
+ # 
+# model_adaboost$importance
+# error_adaboost <- errorevol(model_adaboost,X_test_dataset)
+# x<-c(1:100)
+# ada_table <- rbind(x,error_adaboost$error)
+# ada_table <- t(ada_table)
+# ada_table <- as.data.frame(ada_table)
+# colnames(ada_table) <- c("iteration","error_rate")
+# plot(x=ada_table$iteration,y=ada_table$error_rate)
 
